1c9768de96b2bed1a4f2d4fef46737c11a1796a6
aurelionsulll <mennanamehdi@gmail.com>
2022-01-25 18:53:38 +0100

    slide in hero section

---
+++ components/Hero.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {
+	Pagination,
+	EffectFade,
+	Autoplay,
+	EffectCoverflow,
+} from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-coverflow';
+import Slide from '../components/Slide';
+
+export default class Hero extends Component {
+	constructor() {
+		super();
+		SwiperCore.use([Autoplay, EffectCoverflow]);
+	}
+	render() {
+		return (
+			<div className='absolute top-0 left-0 right-0 -z-50'>
+				<Swiper
+					modules={[Pagination, EffectFade]}
+					spaceBetween={0}
+					slidesPerView={1}
+					loop={true}
+					autoplay={{
+						delay: 3000,
+						disableOnInteraction: false,
+					}}
+					effect='coverflow'
+					coverflowEffect={{
+						rotate: 0,
+						stretch: 0,
+						depth: 360,
+						modifier: 1,
+					}}
+					pagination={{ clickable: true }}
+				>
+					<SwiperSlide>
+						<Slide
+							image='slideA'
+							text='Fermetures, designer de solutions de fermetures personnalisées'
+							dark={false}
+							link='#'
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<Slide
+							image='slideB'
+							text='Trouvez un professionnel pres de ches vous'
+							dark={false}
+							link='#'
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<Slide
+							image='slideC'
+							text='lorem ipsum dollar imppe '
+							dark={false}
+							link='#'
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<Slide
+							image='slideD'
+							text='the greate gastaby by aurelions usl'
+							dark={true}
+							link='#'
+						/>
+					</SwiperSlide>
+				</Swiper>
+			</div>
+		);
+	}
+}
