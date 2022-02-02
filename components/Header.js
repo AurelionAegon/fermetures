c9765d2c2e6d7a7e59411571979869d4cb10b57e
ziko-23 <kabouchziko3@gmail.com>
2022-02-02 19:07:05 +0100

    a to Link tag

--- components/Header.js
+++ components/Header.js
@@ -13,13 +13,13 @@ export default function Header() {
 		<div className='px-10 py-6 bg-white/40 mx-4 rounded-2xl absolute top-0 left-0 right-0 z-10 mt-6'>
 			<div className='flex justify-between items-center'>
 				{/* logo bgin*/}
-				<a href='/' className='flex items-center'>
+				<Link href='/' className='flex items-center'>
 					<img
 						src='/logo.png'
 						className='w-20  object-contain'
 						alt='logo'
 					/>
-				</a>
+				</Link>
 				{/* logo end*/}
 
 				<button
