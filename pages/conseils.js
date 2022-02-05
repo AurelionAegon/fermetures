010fa3c9085d81d93b9e61c6e6c22fd7835a86d7
ziko-23 <kabouchziko3@gmail.com>
2022-02-05 08:17:04 +0100

    content

--- pages/conseils.js
+++ pages/conseils.js
@@ -17,6 +17,7 @@ export default class Conseils extends Component {
           />
           {/* <NosProduits /> */}
           <div className="h-screen">
+            content
           </div>
          
         </div>
